4ef8e896861bf9c2de0c3831d3acaa8f53badcd8
Dave Spencer <david.spencerian@gmail.com>
2021-01-29 11:23:43 -0800

    Update tick.js

--- tick.js
+++ tick.js
@@ -1,17 +1,33 @@
-f1 = document.getElementById('f1');
-f1.innerHTML = 'yes'
+var pct = document.getElementById('pct');
 
 var ticker = 0;
 var start = new Date().getTime();
+var istart = new Date().getTime();
+
+function now1000() {
+  return Date().getTime();
+}
 
 function tick() {
   console.log('tick');
-  var now = new Date().getTime();
+	
+  var now = now1000();
   var dt = now - start;
-  var pp = new Date(dt).toISOString().substr(14, 5)
+  var pp = new Date(dt).toISOString().substr(14, 5);
+	
+  var idt = now - istart;
+  var ipp = new Date(idt).toISOString().substr(14, 5);
+	
   ticker += 1;
-  f1 = document.getElementById('f1');
-	f1.innerHTML = ticker + ' ' + pp;
+  var f1 = document.getElementById('f1');
+  f1.innerHTML = ticker + ' ' + pp;
+	
+  var f2 = document.getElementById('f2');
+  f2.innerHTML = ticker + ' ' + ipp;
+	
+  if (idt >= 10000) {
+    istart = now1000();
+  }
 }
 
 var interval = setInterval(tick, 1000);
