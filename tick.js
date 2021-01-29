5f59f944eb5a61823fa1f2ed0f2e85a3228a80f8
Dave Spencer <david.spencerian@gmail.com>
2021-01-29 11:13:34 -0800

    Create tick.js

---
+++ tick.js
@@ -0,0 +1,32 @@
+f1 = document.getElementById('f1');
+f1.innerHTML = 'yes'
+
+var ticker = 0;
+var start = new Date().getTime();
+
+function tick() {
+  console.log('tick');
+	var now = new Date().getTime();
+  var dt = now - start;
+  var pp = new Date(dt).toISOString().substr(14, 5)
+  ticker += 1;
+  f1 = document.getElementById('f1');
+	f1.innerHTML = ticker + ' ' + pp;
+}
+
+var interval = setInterval(tick, 1000);
+
+function do_start() {
+  if (interval == null) {
+    do_stop();
+  }
+	console.log('really starting');
+	interval = setInterval(tick, 1000)
+}
+
+function do_stop() {
+  console.log('stopping');
+	clearInterval(interval);
+  interval = null;
+ }
+  
